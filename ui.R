4be57c3d3d59510ac429fc2c37ed685d17903d1a
yongyang huo <stephenhuo@hotmail.com>
2015-10-26 16:18:38 +0000

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,25 @@
+ library(shiny) 
+ shinyUI(pageWithSidebar( 
+   headerPanel("Monthly NetPay Calculator"), 
+   sidebarPanel( 
+     h4('Annual Salary'), 
+     numericInput('yearlySalary', 'Total Gross Pay (£)', 30000), 
+    
+     h5('Deductions'), 
+     numericInput('tax', 'Tax (%)', 20), 
+     numericInput('pension', 'Pension (%)', 4), 
+     numericInput('nationalInsurance', 'National Insurance', 10), 
+
+   ), 
+   mainPanel( 
+     h4('Monthly NetPay (£)'), 
+     textOutput("monthlyNetPay"), 
+
+     br(), 
+     h4('Instructions'), 
+     helpText("This application is for people to calculate their monthly Netpay with the Annual Gross Pay. ."), 
+     code("Monthly NetPay"), 
+     helpText("Enter the Annual salary and modify the relate deduction to calculate the monthly netpay.")
+
+   ) 
+ )) 
