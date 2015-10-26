7418724c3a3a9ce67309ca5528a7933d2ac217f1
yongyang huo <stephenhuo@hotmail.com>
2015-10-26 16:19:09 +0000

    Create server.R

---
+++ server.R
@@ -0,0 +1,16 @@
+library(shiny) 
+ shinyServer( 
+   function(input, output){ 
+     output$monthlyNetPay <- renderText({ calculatemonthlyNetPay(input$yearlySalary, input$tax, input$pension, input$nationalInsurance) }) 
+
+   } 
+ ) 
+ 
+ 
+  calculatemonthlyNetPay <- function (yearlySalary, tax, pension, nationalInsurance)  
+ { 
+   result <- (yearlySalary -  yearlySalary*(tax/100) -  yearlySalary*(pension/100)  - yearlySalary*(nationalInsurance/100) ) / 12
+   return(round(result, digits = 2)) 
+ }  
+ 
+ 
